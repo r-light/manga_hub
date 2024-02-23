a2bfc1085fe439104871c833413848aac7138ae6
r-light <414271394@qq.com>
2024-02-23 18:01:37 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/manga_hub/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.manga_hub
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:manga_hub/widgets/my_app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
